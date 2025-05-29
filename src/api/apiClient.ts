975b5e8d9a5a564e423263a7158313834cb7e12f
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-05-30 03:02:17 +0900

    baseUrlを環境変数で指定するように

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import createClient from 'openapi-fetch'
 import type { paths } from '@/api/schema'
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080'
+const baseUrl = import.meta.env.VITE_API_BASE_URL ?? ''
 
 export const apiClient = createClient<paths>({
   baseUrl: baseUrl,
