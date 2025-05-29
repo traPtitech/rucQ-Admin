d2e0430d9c564797d4590edbefb956db9ddcc5b1
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-05-29 16:47:40 +0900

    localhostのポート番号変更

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import createClient from 'openapi-fetch'
 import type { paths } from '@/api/schema'
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000'
+const baseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080'
 
 export const apiClient = createClient<paths>({
   baseUrl: baseUrl,
