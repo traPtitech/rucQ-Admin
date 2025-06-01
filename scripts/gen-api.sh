65b741f75fe770104eb4fd144e5ab023074ca1fe
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-06-01 16:19:02 +0900

    schema.d.ts生成スクリプトをpackage.jsonに移動

--- scripts/gen-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-INPUT_FILE="https://raw.githubusercontent.com/traPtitech/rucQ/main/openapi.yaml"
-OUTPUT_FILE="./src/api/schema.d.ts"
-
-npx openapi-typescript $INPUT_FILE -o $OUTPUT_FILE
-npm run format
