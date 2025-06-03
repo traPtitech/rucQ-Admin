440c710460501be261cbe3e5aca5472a6da3f5b0
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-06-04 03:05:59 +0900

    TopViewの変更を戻した

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -1,22 +1,7 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { apiClient } from '@/api/apiClient'
-import type { components } from '@/api/schema'
-
-const data = ref<components['schemas']['Camp'][]>([])
-
-onMounted(async () => {
-  data.value = (await apiClient.GET('/api/camps')).data ?? []
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <v-container>
-    {{ data.length }}
-    <ul>
-      <li v-for="camp in data" :key="camp.id">
-        {{ camp.name }}
-      </li>
-    </ul>
-  </v-container>
+  <v-container> Top </v-container>
 </template>
+
+<style module></style>
