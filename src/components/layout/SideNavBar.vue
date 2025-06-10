0013be1fb1c5394d9b9041c94bb4b596d9329eee
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-06-11 05:10:46 +0900

    ナビゲーションバーを実装

---
+++ src/components/layout/SideNavBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NavItem } from '@/components/layout/NavigationBar.vue'
+
+defineProps<{
+  navs: NavItem[]
+}>()
+</script>
+
+<template>
+  <v-navigation-drawer permanent>
+    <v-list-item
+      v-for="nav in navs"
+      :key="nav.title"
+      :prepend-icon="nav.icon"
+      :title="nav.title"
+      :to="nav.to"
+    />
+  </v-navigation-drawer>
+</template>
---
+++ src/components/layout/TopNavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const title = computed(() => route.meta.title || 'Unknown Page')
+</script>
+
+<template>
+  <v-app-bar scroll-behavior="hide">
+    <v-app-bar-title>{{ title }}</v-app-bar-title>
+  </v-app-bar>
+</template>
