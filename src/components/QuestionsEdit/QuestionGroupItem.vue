cb58dfe369456225f390cb8cccbd3acaa8b3fdab
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-06-28 21:39:18 +0900

    一旦編集に関わる部分を削除

--- src/components/QuestionsEdit/QuestionGroupItem.vue
+++ src/components/QuestionsEdit/QuestionGroupItem.vue
@@ -1,28 +1,14 @@
 <script setup lang="ts">
 import QuestionItem from '@/components/QuestionsEdit/QuestionItem.vue'
-import { ref } from 'vue'
-import { apiClient } from '@/api/apiClient'
 import type { components } from '@/api/schema'
 
-const props = defineProps<{
+defineProps<{
   questionGroup: components['schemas']['QuestionGroupResponse']
 }>()
-
-const editMode = ref(false)
-
-const editedQuestionGroup = ref<components['schemas']['QuestionGroupResponse']>(props.questionGroup)
-
-const saveQuestionGroup = async () => {
-  apiClient.PUT('/api/admin/question-groups/{questionGroupId}', {
-    params: { path: { questionGroupId: props.questionGroup.id } },
-    body: editedQuestionGroup.value,
-  })
-  // TODO: 質問追加にも対応
-}
 </script>
 
 <template>
-  <v-sheet v-if="!editMode" elevation="2" class="d-flex flex-column ga-2 pa-4">
+  <v-sheet elevation="2" class="d-flex flex-column ga-2 pa-4">
     <div class="mb-2">
       <h2>{{ questionGroup.name }}</h2>
       <p>{{ questionGroup.description }}</p>
@@ -34,25 +20,6 @@ const saveQuestionGroup = async () => {
         :question="question"
       />
     </div>
-    <v-btn color="primary" class="mt-2 w-auto align-self-end" @click="editMode = true">
-      編集
-    </v-btn>
-  </v-sheet>
-
-  <v-sheet v-else elevation="4" class="d-flex flex-column ga-2 pa-4">
-    <div class="mb-2">
-      <h2>{{ editedQuestionGroup.name }}</h2>
-      <p>{{ editedQuestionGroup.description }}</p>
-    </div>
-    <div class="d-flex flex-column ga-2">
-      <question-item
-        v-for="question in editedQuestionGroup.questions"
-        :key="question.id"
-        :question="question"
-      />
-    </div>
-    <v-btn color="primary" class="mt-2 w-auto align-self-end" @click="saveQuestionGroup">
-      保存
-    </v-btn>
+    <v-btn color="primary" class="mt-2 w-auto align-self-end"> 編集 </v-btn>
   </v-sheet>
 </template>
--- src/views/QuestionsEditView.vue
+++ src/views/QuestionsEditView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import QuestionGroupItem from '@/components/QuestionsEdit/QuestionGroupItem.vue'
-import CreateQuestionGroupButton from '@/components/QuestionsEdit/CreateQuestionGroupButton.vue'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { apiClient } from '@/api/apiClient'
@@ -25,72 +24,6 @@ const fetchQuestionGroups = async () => {
   questionGroups.value = data ?? []
 }
 
-const updateQuestionGroupSummary = async (
-  questionGroupId: number,
-  questionGroupSummary: components['schemas']['QuestionGroupRequest'],
-) => {
-  const index = questionGroups.value.findIndex((qg) => qg.id === questionGroupId)
-  if (index === -1) return
-  const { data, error } = await apiClient.PUT('/api/admin/question-groups/{questionGroupId}', {
-    params: { path: { questionGroupId: questionGroupId } },
-    body: questionGroupSummary,
-  })
-  if (error) console.error('Error updating question group summary:', error)
-  else questionGroups.value[index] = data
-}
-
-const updateQuestion = async (question: components['schemas']['QuestionResponse']) => {
-  const { data, error } = await apiClient.PUT('/api/admin/questions/{questionId}', {
-    params: { path: { questionId: question.id } },
-    body: question,
-  })
-  if (error) {
-    console.error('Error updating question:', error)
-    return
-  }
-  const groupIndex = questionGroups.value.findIndex((qg) => qg.id === question.questionGroupId)
-  if (groupIndex !== -1) {
-    const questionIndex = questionGroups.value[groupIndex].questions.findIndex(
-      (q) => q.id === question.id,
-    )
-    if (questionIndex !== -1) {
-      questionGroups.value[groupIndex].questions[questionIndex] = data
-    }
-  }
-}
-
-const createQuestion = async (newQuestion: components['schemas']['QuestionRequest']) => {
-  const index = questionGroups.value.findIndex((qg) => qg.id === newQuestion.questionGroupId)
-  if (index !== -1) return
-  const { data, error } = await apiClient.POST('/api/admin/questions', { body: newQuestion })
-  if (error) {
-    console.error('Error creating question:', error)
-    return
-  }
-  questionGroups.value[index].questions.push(data)
-}
-
-const updateQuestionGroup = async (
-  questionGroup: components['schemas']['QuestionGroupResponse'],
-  newQuestions: components['schemas']['QuestionRequest'][],
-) => {
-  await updateQuestionGroupSummary(questionGroup.id, questionGroup)
-  for (const question of questionGroup.questions) await updateQuestion(question)
-  for (const newQuestion of newQuestions) await createQuestion(newQuestion)
-  fetchQuestionGroups()
-}
-
-const deleteQuestionGroup = async (questionGroupId: number) => {
-  const index = questionGroups.value.findIndex((qg) => qg.id === questionGroupId)
-  if (index === -1) return
-  const { error } = await apiClient.DELETE('/api/admin/question-groups/{questionGroupId}', {
-    params: { path: { questionGroupId: questionGroupId } },
-  })
-  if (error) console.error('Error deleting question group:', error)
-  else questionGroups.value.splice(index, 1)
-  fetchQuestionGroups()
-}
-
 onMounted(async () => {
   await fetchCamp()
   await fetchQuestionGroups()
@@ -99,13 +32,6 @@ onMounted(async () => {
 
 <template>
   <v-container max-width="800" class="d-flex flex-column justify-center ga-6">
-    <question-group-item
-      v-for="group in questionGroups"
-      :key="group.id"
-      :question-group="group"
-      @update-question-group="updateQuestionGroup"
-      @delete-question-group="deleteQuestionGroup"
-    />
-    <create-question-group-button />
+    <question-group-item v-for="group in questionGroups" :key="group.id" :question-group="group" />
   </v-container>
 </template>
