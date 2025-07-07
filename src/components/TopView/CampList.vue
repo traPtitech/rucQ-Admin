39e4c87c12024d17cd72f7a4b26d9c3551451337
ErrorSyntax1 <ken522.king@gmail.com>
2025-07-08 00:58:10 +0900

    descriptionからの修正漏れを修正

--- src/components/TopView/CampList.vue
+++ src/components/TopView/CampList.vue
@@ -12,7 +12,7 @@ const { title, camps } = defineProps<{
     <v-card v-for="camp in camps" :key="camp.id" class="ma-4">
       <v-card-title>{{ camp.name }}</v-card-title>
       <v-card-subtitle>{{ camp.dateStart }} 〜 {{ camp.dateEnd }}</v-card-subtitle>
-      <v-card-text>{{ camp.description }}</v-card-text>
+      <v-card-text>{{ camp.guidebook }}</v-card-text>
       <v-card-actions>
         <v-btn color="primary" :to="`/${camp.displayId}`">詳細</v-btn>
       </v-card-actions>
