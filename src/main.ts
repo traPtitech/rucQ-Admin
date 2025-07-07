60d07e9cb65e546aa24237762592bab498c09662
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-07-08 00:20:52 +0900

    トップレベルawaitの解消

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { vuetify } from './plugins/vuetify'
+import { worker } from './mocks/browser'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -13,8 +14,7 @@ app.use(vuetify)
 
 // MSW
 if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser')
-  await worker.start()
+  worker.start()
 }
 
 app.mount('#app')
