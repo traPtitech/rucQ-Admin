bf679bf67d39948895b5779be058c14a646ceda1
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-07-10 12:59:05 +0900

    ツール一覧画面を実装

---
+++ src/components/Tools/ToolCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { RouteLocationAsRelativeGeneric } from 'vue-router'
+defineProps<{
+  title: string
+  icon: string
+  to: RouteLocationAsRelativeGeneric
+}>()
+</script>
+
+<template>
+  <v-card
+    class="d-flex flex-column align-center ga-1 pa-4"
+    elevation="2"
+    rounded="0"
+    :to="to"
+  >
+    <v-icon size="48" :icon="icon" />
+    <h3 class="text-h6">{{ title }}</h3>
+  </v-card>
+</template>
--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,5 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ToolCard from '@/components/Tools/ToolCard.vue'
+import { useRoute, type RouteLocationAsRelativeGeneric } from 'vue-router'
+
+interface Tool {
+  title: string
+  icon: string
+  to: RouteLocationAsRelativeGeneric
+}
+
+const route = useRoute()
+const campname = route.params.campname as string
+
+const tools: Tool[] = [
+  {
+    title: 'トップ',
+    icon: 'mdi-home',
+    to: { name: 'Top' },
+  },
+  {
+    title: '質問編集',
+    icon: 'mdi-file-question',
+    to: { name: 'QuestionsEdit', params: { campname } },
+  },
+  {
+    title: '部屋情報',
+    icon: 'mdi-door',
+    to: { name: 'Rooms', params: { campname } },
+  },
+  {
+    title: '点呼',
+    icon: 'mdi-account-check',
+    to: { name: 'RollCallList', params: { campname } },
+  },
+  {
+    title: '支払い情報',
+    icon: 'mdi-cash',
+    to: { name: 'Payments', params: { campname } },
+  },
+]
+</script>
 
 <template>
-  <v-container> Tools </v-container>
+  <v-container>
+    <v-row class="pa-3">
+      <v-col v-for="tool in tools" :key="tool.title" cols="6" md="4">
+        <ToolCard :title="tool.title" :icon="tool.icon" :to="tool.to" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
