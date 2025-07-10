03044b1c496dbf925d7c8ed1709ba646d62c883e
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-07-10 13:00:51 +0900

    スタイルの調整

--- src/components/Tools/ToolCard.vue
+++ src/components/Tools/ToolCard.vue
@@ -8,12 +8,7 @@ defineProps<{
 </script>
 
 <template>
-  <v-card
-    class="d-flex flex-column align-center ga-1 pa-4"
-    elevation="2"
-    rounded="0"
-    :to="to"
-  >
+  <v-card class="d-flex flex-column align-center ga-1 pa-4" elevation="2" rounded="0" :to="to">
     <v-icon size="48" :icon="icon" />
     <h3 class="text-h6">{{ title }}</h3>
   </v-card>
