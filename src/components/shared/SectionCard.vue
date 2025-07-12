f1868a191be088033ce06f515791333678729e39
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-07-12 23:09:54 +0900

    汎用コンポーネントを用意

---
+++ src/components/shared/SectionCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const { elevation = 2 } = defineProps<{
+  elevation?: number | string
+}>()
+
+const elevationValue = computed(() => {
+  return typeof elevation === 'number' ? elevation : parseInt(elevation, 10)
+})
+</script>
+
+<template>
+  <v-sheet class="pa-4 mb-6" :elevation="elevationValue">
+    <slot />
+  </v-sheet>
+</template>
