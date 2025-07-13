8ee81dddc0c5fe41e9b053831bde5b699f2cdafc
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-07-13 23:58:05 +0900

    ContentCardに明示的にtoプロパティを追加

--- src/components/shared/ContentCard.vue
+++ src/components/shared/ContentCard.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useTheme } from 'vuetify'
+import type { RouteLocationAsRelativeGeneric } from 'vue-router'
 
 const { borderColor = 'primary', elevation = 2 } = defineProps<{
   borderColor?: string
   elevation?: number | string
+  to?: RouteLocationAsRelativeGeneric
 }>()
 
 const theme = useTheme()
@@ -18,7 +20,7 @@ const elevationValue = computed(() => {
 </script>
 
 <template>
-  <v-card class="question-sheet px-4 py-2 my-2" :elevation="elevationValue" rounded>
+  <v-card class="question-sheet px-4 py-2 my-2" :to="to" :elevation="elevationValue" rounded>
     <slot />
   </v-card>
 </template>
