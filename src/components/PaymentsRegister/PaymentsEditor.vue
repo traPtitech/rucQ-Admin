775e689d0b51e7949730de4962bef16f66100295
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-08-16 23:25:01 +0900

    概要セクションを実装

---
+++ src/components/PaymentsRegister/PaymentsEditor.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SectionCard from '@/components/shared/SectionCard.vue'
+</script>
+
+<template>
+  <section-card> Payments Editor </section-card>
+</template>
--- src/views/PaymentsRegisterView.vue
+++ src/views/PaymentsRegisterView.vue
@@ -1,7 +1,84 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import SectionTitle from '@/components/shared/SectionTitle.vue'
+import PaymentsSummary from '@/components/PaymentsRegister/PaymentsSummary.vue'
+import PaymentsEditor from '@/components/PaymentsRegister/PaymentsEditor.vue'
+import { apiClient } from '@/api/apiClient'
+import type { components } from '@/api/schema'
+type Camp = components['schemas']['CampResponse']
+type User = components['schemas']['UserResponse']
+type Payment = components['schemas']['PaymentResponse']
+// type PaymentRequest = components['schemas']['PaymentRequest']
+
+const route = useRoute()
+const campname = route.params.campname as string
+const camp = ref<Camp>()
+const participants = ref<User[]>([])
+const payments = ref<Payment[]>([])
+
+const fetchCamp = async () => {
+  const { data } = await apiClient.GET('/api/camps')
+  return data?.find((camp) => camp.displayId === campname)
+}
+
+const fetchParticipants = async () => {
+  if (!camp.value) return
+  const { data } = await apiClient.GET('/api/camps/{campId}/participants', {
+    params: { path: { campId: camp.value.id } },
+  })
+  return data
+}
+
+const fetchPayments = async () => {
+  if (!camp.value) return
+  const { data } = await apiClient.GET('/api/admin/camps/{campId}/payments', {
+    params: { path: { campId: camp.value.id } },
+  })
+  return data
+}
+
+// const createPayment = async (payment: PaymentRequest) => {
+//   if (!camp.value) return
+//   const { data, error } = await apiClient.POST('/api/admin/camps/{campId}/payments', {
+//     params: { path: { campId: camp.value.id } },
+//     body: payment,
+//   })
+//   if (error) {
+//     alert(`@${payment.userId}の支払い情報の登録に失敗しました: ${error.message}`)
+//     return
+//   }
+//   payments.value.push(data)
+// }
+
+// const updatePayment = async (id: number, payment: PaymentRequest) => {
+//   if (!camp.value) return
+//   const { data, error } = await apiClient.PUT('/api/admin/payments/{paymentId}', {
+//     params: { path: { paymentId: id } },
+//     body: payment,
+//   })
+//   if (error) {
+//     alert(`@${payment.userId}の支払い情報の更新に失敗しました: ${error.message}`)
+//     return
+//   }
+//   const index = payments.value.findIndex((p) => p.id === id)
+//   if (index !== -1) {
+//     payments.value[index] = data
+//   }
+// }
+
+onMounted(async () => {
+  camp.value = await fetchCamp()
+  participants.value = (await fetchParticipants()) ?? []
+  payments.value = (await fetchPayments()) ?? []
+})
+</script>
 
 <template>
-  <v-container> Payments Register </v-container>
+  <v-container max-width="800">
+    <section-title title="概要" />
+    <payments-summary :participants="participants" :payments="payments" />
+    <section-title title="登録" />
+    <payments-editor />
+  </v-container>
 </template>
-
-<style module></style>
