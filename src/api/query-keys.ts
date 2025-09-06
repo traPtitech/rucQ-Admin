f6c5442770ae4d125bdda86a2c2ad0401d886b27
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-09-06 21:30:21 +0900

    queryKeyの引数の表記を統一

--- src/api/query-keys.ts
+++ src/api/query-keys.ts
@@ -11,27 +11,27 @@ export const queryKeys = {
     detail: (userId: string) => [...queryKeys.users.all, userId] as const,
   },
   events: {
-    all: (campId: number | undefined) => ['events', campId] as const,
+    all: (campId?: number) => ['events', campId] as const,
   },
   participants: {
-    all: (campId: number | undefined) => ['participants', campId] as const,
+    all: (campId?: number) => ['participants', campId] as const,
   },
   payments: {
-    all: (campId: number | undefined) => ['payments', campId] as const,
+    all: (campId?: number) => ['payments', campId] as const,
   },
   questionGroups: {
-    all: (campId: number | undefined) => ['questionGroups', campId] as const,
+    all: (campId?: number) => ['questionGroups', campId] as const,
   },
   roomGroups: {
-    all: (campId: number | undefined) => ['roomGroups', campId] as const,
+    all: (campId?: number) => ['roomGroups', campId] as const,
   },
   answers: {
     root: ['answers'] as const,
     lists: () => [...queryKeys.answers.root, 'list'] as const,
-    list: (questionId: number | undefined, userId?: string) =>
+    list: (questionId?: number, userId?: string) =>
       [...queryKeys.answers.lists(), questionId, userId] as const,
     groups: () => [...queryKeys.answers.root, 'groups'] as const,
-    group: (questionGroupId: number | undefined, userId?: string) =>
+    group: (questionGroupId?: number, userId?: string) =>
       [...queryKeys.answers.groups(), questionGroupId, userId] as const,
   },
 }
